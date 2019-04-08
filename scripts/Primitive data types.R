7696a76a53d71a3a224a8e731640a211d2987bf5
mukundajmera <mukundajmera94@gmail.com>
2019-04-08 10:28:00 +0530

    Adding basic data types

---
+++ scripts/Primitive data types.R
@@ -0,0 +1,33 @@
+#Premitive data types
+
+#Numeric
+#Integer
+#Character
+#Boolean/logical
+
+#Numeric
+
+a = 15
+class(a)
+typeof(a)
+is.numeric(a)
+
+#Integer
+b = 20L
+class(b)
+typeof(b)
+is.integer(b)
+
+#Character
+
+c = "Hello"
+class(c)
+typeof(c)
+is.character(c)
+
+#Boolean/Logical
+d = TRUE
+
+class(d)
+typeof(d)
+is.logical(d)
